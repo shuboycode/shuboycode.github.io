f31bbfdf2fafc38972e1e65d7b20ee3d87409ba0
Shubham Kumar <61133619+shuboycode@users.noreply.github.com>
2020-04-18 12:47:27 +0530

    Updated

---
+++ gnduranker/gnduranker/gndu/bca/bca.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!-- Mirrored from a2zpapers.com/gndu/bca/bca.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Apr 2020 07:48:51 GMT -->
+<head>
+<!--mobile ads--->
+<script async src="../../../pagead2.googlesyndication.com/pagead/js/f.txt"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8598336974807399",
+    enable_page_level_ads: true
+  });
+</script>
+<!--mobile ads end--->
+
+
+<title>GNDU Question Papers, BCA Question papers</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="../../style.css" rel="stylesheet" type="text/css" />
+<link rel="icon" type="image/jpg" sizes="192x192" href="../../images/logo.jpg">
+<!-- CuFon: Enables smooth pretty custom font rendering. 100% SEO friendly. To disable, remove this section -->
+<script type="text/javascript" src="../../js/cufon-yui.js"></script>
+<script type="text/javascript" src="../../js/arial.js"></script>
+<script type="text/javascript" src="../../js/cuf_run.js"></script>
+<!-- CuFon ends -->
+<script>
+function goBack() {
+    window.history.back();
+}
+</script>
+</head>
+<body>
+<?php include('include/Header.php'); ?>
+
+<div class="main">
+
+  <div class="header">
+    
+  <div class="clr"></div>
+
+
+  <div class="hbg"></div>
+  
+  <!-- google ad-->
+  <center>  <script async src="../../../pagead2.googlesyndication.com/pagead/js/f.txt"></script>
+<!-- a2zpaper1 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-8598336974807399"
+     data-ad-slot="1004327936"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script> </center>
+<!--google ad end-->
+
+  <div class="content">
+    <div class="content_resize">
+      <div class="mainbar">
+        <div class="article">         
+          <h2>GNDU,BCA</h2>          
+          <ul class="sb_menu">          
+         <li> <h3><a href="bca1.html">SEMESTER–I</a></h3></li>
+          <li> <h3><a href="bca2.html">SEMESTER–II</a></h3></li>
+         <li><h3><a href="bca3.html">SEMESTER–III</a></h3></li>
+         <li> <h3><a href="bca4.html">SEMESTER–IV</a> </h3></li>
+         <li> <h3><a href="bca5.html">SEMESTER–V</a> </h3></li>
+         <li> <h3> <a href="bca6.html">SEMESTER–VI</a></h3></li>
+         
+         
+   
+</ul>
+<br />
+         <br />
+         <center><img src="../../images/back-button.png" width="100px" height="40" onclick="goBack()"/></center>
+          
+        </div>
+        
+        <!--google ad-->
+        <center><script async src="../../../pagead2.googlesyndication.com/pagead/js/f.txt"></script>
+<!-- a2zpapers4 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:250px"
+     data-ad-client="ca-pub-8598336974807399"
+     data-ad-slot="7326672633"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></center>
+<!--google ad end-->
+
+        <div class="article">
+          <h2>&nbsp;</h2>
+        </div>
+        <div class="article">
+          <h2>&nbsp;</h2>
+        </div>
+      </div>
+
+      <div class="sidebar">
+        
+        
+        <!--google ad-->
+        <center><script async src="../../../pagead2.googlesyndication.com/pagead/js/f.txt"></script>
+<!-- a2zpapers3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8598336974807399"
+     data-ad-slot="2449271542"
+     data-ad-format="link"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></center>
+<!--google ad end-->
+
+        
+      
+  </div>
+  
+<!-- google ad-->
+  <center>  <script async src="../../../pagead2.googlesyndication.com/pagead/js/f.txt"></script>
+<!-- a2zpaper1 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-8598336974807399"
+     data-ad-slot="1004327936"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script> </center>
+<!--google ad end-->
+
+  <div class="fbg">
+   
+      
+      
+      
+      
+    </div>
+    <div class="clr"></div>
+  </div>
+</div>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/591c12584ac4446b24a6f90a/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+</body>
+
+<!-- Mirrored from a2zpapers.com/gndu/bca/bca.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Apr 2020 07:48:55 GMT -->
+</html>
